a04cc02478d643953edf94ee56de684ee03123a2
Andy Song <congxisong@hotmail.com>
2024-10-31 16:01:34 +0000

    çommit before tring dataset2.0

--- graphical_model/within_basket_embedding.py
+++ graphical_model/within_basket_embedding.py
@@ -11,18 +11,79 @@ import warnings
 warnings.filterwarnings('ignore')
 import pandas as pd
 import numpy as np
+import dgl.function as fn
 from utils import load_data,get_label
 from torch.utils.data import Dataset,DataLoader
 from torch import nn
 from pandas.api.types import is_float_dtype
 from collections import defaultdict
 
+class WeightedGraphConv(nn.Module):
+    def __init__(self,
+                 in_features,
+                 out_features):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.linear = nn.Linear(in_features,out_features,bias=True)
+    
+    def forward(self,graph,node_features,edge_weights):
+        graph = graph.local_var()
+        graph = graph.to('cuda')
+        graph.ndata['n'] = node_features
+        graph.edata['e'] = edge_weights.t().unsqueeze(-1)
+        graph.update_all(fn.u_mul_e('n','e','msg'),fn.sum('msg','h'))
+        node_features = graph.node.pop('h')
+        output = self.linear(node_features)
+        return output
+
+class WeightedGCN(nn.Module):
+    def __init__(self,
+                 in_features,
+                 hidden_sizes,
+                 out_features):
+        super().__init__()
+        gcns,relus,bns = nn.ModuleList(),nn.ModuleList(),nn.ModuleList()
+        for hidden_size in hidden_sizes:
+            gcns.append(WeightedGraphConv(in_features,hidden_size))
+            relus.append(nn.ReLU())
+            bns.append(nn.BatchNorm1d(hidden_size))
+            in_features = hidden_size
+        
+        gcns.append(WeightedGraphConv(hidden_sizes[-1], out_features))
+        relus.append(nn.ReLU())
+        bns.append(nn.BatchNorm1d(hidden_size))
+        self.gcns,self.relus,self.bns = gcns,relus,bns
+    
+    def forward(self,graph,node_features,edge_weights):
+        h = node_features
+        for gcn,relu,bn in zip(self.gcns,self.relus,self.bns):
+            h = gcn(graph,h,edge_weights)
+            h = bn(h.transpose(1,-1)).transpose(1,-1)
+            h = relu(h)
+        return h
+    
 class DNNSTP(nn.Module):
     def __init__(self,
                  n_items,
                  emb_dim):
         super().__init__()
         self.item_embedding = nn.Embedding(n_items,emb_dim)
+        self.n_items = n_items
+        self.gcn = WeightedGCN(emb_dim,[emb_dim],emb_dim)
+    
+    def forward(
+        self,
+        batch_graph,
+        batch_nodes_feature,
+        batch_edges_weight,
+        batch_lengths,
+        batch_nodes,
+    ):
+        # batch_nodes_output = [self.gcn(graph,nodes,weights) for graph,nodes,weights in zip(batch_graph,batch_nodes_feature,batch_edges_weight)]
+        batch_nodes_output = self.gcn(batch_graph,batch_nodes_feature,batch_nodes_feature)
+        return batch_nodes_output
+        
 
 class DnnstpSet(Dataset):
     def __init__(self,
@@ -58,7 +119,7 @@ class DnnstpSet(Dataset):
         user_baskets = self.user_products[index+1]
         
         unique_items = torch.unique(torch.tensor(list(itertools.chain.from_iterable(user_baskets[:-1]))))
-        products_embedding = self.item_embedding(unique_items)
+        products_embedding = self.item_embedding(unique_items.to("cuda"))
         nodes = torch.tensor(list(range(products_embedding.shape[0])))
         src = np.stack([nodes for _ in range(nodes.shape[0])],axis=1).flatten().tolist()
         dst = np.stack([nodes for _ in range(nodes.shape[0])],axis=0).flatten().tolist()
@@ -108,7 +169,6 @@ def collate(batch):
                 edge_weights,data_lengths = [],[]
                 for d in data:
                     if d.shape[0] < max_length:
-                        print(d)
                         edge_weights.append(torch.cat((d,torch.stack([torch.eye(int(d.shape[1]**0.5)).flatten() \
                                               for _ in range(max_length - d.shape[0])],dim=0)),dim=0))
                     else:
@@ -135,23 +195,25 @@ def collate(batch):
     return ret
 
 if __name__ == '__main__':
-    # model = DNNSTP(49688,100)
-    # dataset = DnnstpSet('data/',model.item_embedding)
+    model = DNNSTP(49688,100).to('cuda')
+    dataset = DnnstpSet('data/',model.item_embedding)
     dl = DataLoader(dataset=dataset,
                     batch_size=8,
                     shuffle=True,
-                    drop_last=False,
-                    collate_fn=collate)
+                    drop_last=False)
+                    # collate_fn=collate)
     cnt = 0
     for batch in dl:
         cnt += 1
+        graph,nodes_features,edge_weights,lengths,nodes,_,_ = batch
         if cnt == 1:
+            result = model(graph,nodes_features,edge_weights,lengths,nodes)
             break
                 
 
 
 #%%
-batch[6]
+batch[2].shape
 
 
 #%%
@@ -165,7 +227,7 @@ batch[6]
 #%%
 # z = np.array(ds[123][2])
 # ds[123][-1]
-len(batch)
+
 
 
 
