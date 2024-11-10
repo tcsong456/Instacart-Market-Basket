e1a4771be1e6cb232b85d74a6e69dc99e8c03a75
Andy Song <congxisong@hotmail.com>
2024-11-10 19:59:00 +0000

    solve dataframe complex datatypes issue

--- embedding/temporal_products.py
+++ embedding/temporal_products.py
@@ -16,24 +16,31 @@ from nn_model.lstm import ProdLSTM
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-def data_processing(data):
-    data = data.sort_values(['user_id','order_number','add_to_cart_order'])
-    grouped_data = []
-    
-    for col in ['product_id','reordered','aisle_id','department_id']:
-        x = data.groupby(['user_id','order_id','order_number'])[col].apply(list).map(lambda x:'_'.join(map(str,x))).reset_index()
-        x = x.sort_values(['user_id','order_number']).groupby(['user_id'])[col].apply(list)
-        grouped_data.append(x)
-    r = pd.concat(grouped_data,axis=1)
-    
-    grouped_attr = []
-    for col in ['order_hour_of_day','order_dow','days_since_prior_order','time_zone']:
-        x = data.groupby(['user_id','order_id','order_number']).apply(lambda x:x[col].iloc[0])
-        x.name = col
-        x = x.reset_index().sort_values(['user_id','order_number']).groupby('user_id')[col].apply(list).map(lambda x:'_'.join(map(str,x)))
-        grouped_attr.append(x)
-    x = pd.concat(grouped_attr,axis=1)
-    r = r.merge(x,how='left',on='user_id').reset_index()
+def data_processing(data,save=False):
+    path = 'data/user_product_info.csv'
+    try:
+        r = pd.read_pickle(path)
+    except FileNotFoundError:
+        logger.info('building user data')
+        data = data.sort_values(['user_id','order_number','add_to_cart_order'])
+        grouped_data = []
+        
+        for col in ['product_id','reordered','aisle_id','department_id']:
+            x = data.groupby(['user_id','order_id','order_number'])[col].apply(list).map(lambda x:'_'.join(map(str,x))).reset_index()
+            x = x.sort_values(['user_id','order_number']).groupby(['user_id'])[col].apply(list)
+            grouped_data.append(x)
+        r = pd.concat(grouped_data,axis=1)
+        
+        grouped_attr = []
+        for col in ['order_hour_of_day','order_dow','days_since_prior_order','time_zone']:
+            x = data.groupby(['user_id','order_id','order_number']).apply(lambda x:x[col].iloc[0])
+            x.name = col
+            x = x.reset_index().sort_values(['user_id','order_number']).groupby('user_id')[col].apply(list).map(lambda x:'_'.join(map(str,x)))
+            grouped_attr.append(x)
+        x = pd.concat(grouped_attr,axis=1)
+        r = r.merge(x,how='left',on='user_id').reset_index()
+        if save:
+            r.to_pickle(path)
     return r
 
 convert_format = lambda x:list(map(int,x.split('_')))
@@ -49,69 +56,71 @@ def oh_encoding(target,num_cates):
 def data_for_training(data,max_len):
     data_dict = {}
     info = []
-    for _,row in data.iterrows():
-        user,products = row['user_id'],row['product_id']
-        reorders = row['reordered']
-        aisles,departments = row['aisle_id'],row['department_id']
-        orders = [product.split('_') for product in products]
-        products,next_products = products[:-1],products[-1]
-        reorders = reorders[:-1]
-        reorders = [list(map(int,reorder.split('_'))) for reorder in reorders]
-        all_products = [list(set(chain.from_iterable(product.split('_') for product in products)))][0]
-        
-        for product in all_products:
-            label = product in next_products
-            info.append([user,int(product),label])
+    with tqdm(total=data.shape[0],desc='building user data for dataloader') as pbar:
+        for _,row in data.iterrows():
+            user,products = row['user_id'],row['product_id']
+            reorders = row['reordered']
+            orders = [product.split('_') for product in products]
+            products,next_products = products[:-1],products[-1]
+            reorders_ = reorders[:-1]
             
-            reorder_cnt = 0
-            in_order_ord = []
-            index_ord = []
-            index_ratio_ord = []
-            reorder_prod_ord = []
-            reorder_prod_ratio_ord = []
-            order_size_ord = []
-            reorder_ord = []
-            reorder_ratio_ord = []
-            for idx,(order,reorder,aisle,department) in enumerate(zip(orders,reorders,aisles,departments)):
-                in_order = int(product in order)
-                index_in_order = order.index(product) + 1 if in_order else 0
-                order_size = len(order)
-                index_order_ratio = index_in_order / order_size
-                reorder_cnt += int(in_order)
-                reorder_ratio_cum = reorder_cnt / (idx+1)
-                reorder_size = sum(reorder)
-                reorder_ratio = reorder_size / order_size
+            reorders_ = [list(map(int,reorder.split('_'))) for reorder in reorders_]
+            all_products = [list(set(chain.from_iterable(product.split('_') for product in products)))][0]
+            
+            for product in all_products:
+                label = product in next_products
+                info.append([user,int(product),label])
                 
-                in_order_ord.append(in_order)
-                order_size_ord.append(order_size)
-                index_ord.append(index_in_order)
-                index_ratio_ord.append(index_order_ratio)
-                reorder_prod_ord.append(reorder_cnt)
-                reorder_prod_ratio_ord.append(reorder_ratio_cum)
-                reorder_ord.append(reorder_size)
-                reorder_ratio_ord.append(reorder_ratio)
-            
-            next_order_label = np.roll(in_order_ord,-1).reshape(-1,1)
-            
-            order_dow = oh_encoding(np.roll(convert_format(row['order_dow']),-1)[:-1],7)
-            order_hour = oh_encoding(np.roll(convert_format(row['order_hour_of_day']),-1)[:-1],24)
-            days_interval = np.roll(convert_format(row['days_since_prior_order']),-1)[:-1]
-            tz = oh_encoding(np.roll(convert_format(row['time_zone']),-1)[:-1],28)
-            prod_info = np.stack([in_order_ord,order_size_ord,index_ord,index_ratio_ord,
-                                  reorder_prod_ord,reorder_prod_ratio_ord,reorder_ord,reorder_ratio_ord,
-                                  days_interval]).transpose()
-            prod_info = np.concatenate([prod_info,order_dow,order_hour,tz,next_order_label],axis=1).astype(np.float16)
-            length = prod_info.shape[0]
-            feature_dim = prod_info.shape[-1] - 1
-            missing_seq = max_len - length
-            if missing_seq > 0:
-                missing_data = np.zeros([missing_seq,prod_info.shape[1]],dtype=np.float16)
-                prod_info = np.concatenate([prod_info,missing_data])
-            
-            data_dict[(user,int(product))] = (prod_info,length)
+                reorder_cnt = 0
+                in_order_ord = []
+                index_ord = []
+                index_ratio_ord = []
+                reorder_prod_ord = []
+                reorder_prod_ratio_ord = []
+                order_size_ord = []
+                reorder_ord = []
+                reorder_ratio_ord = []
+                for idx,(order,reorder) in enumerate(zip(orders,reorders_)):
+                    in_order = int(product in order)
+                    index_in_order = order.index(product) + 1 if in_order else 0
+                    order_size = len(order)
+                    index_order_ratio = index_in_order / order_size
+                    reorder_cnt += int(in_order)
+                    reorder_ratio_cum = reorder_cnt / (idx+1)
+                    reorder_size = sum(reorder)
+                    reorder_ratio = reorder_size / order_size
+                    
+                    in_order_ord.append(in_order)
+                    order_size_ord.append(order_size)
+                    index_ord.append(index_in_order)
+                    index_ratio_ord.append(index_order_ratio)
+                    reorder_prod_ord.append(reorder_cnt)
+                    reorder_prod_ratio_ord.append(reorder_ratio_cum)
+                    reorder_ord.append(reorder_size)
+                    reorder_ratio_ord.append(reorder_ratio)
+                
+                next_order_label = np.roll(in_order_ord,-1).reshape(-1,1)
+                
+                # order_dow = oh_encoding(np.roll(convert_format(row['order_dow']),-1)[:-1],7)
+                # order_hour = oh_encoding(np.roll(convert_format(row['order_hour_of_day']),-1)[:-1],24)
+                # days_interval = np.roll(convert_format(row['days_since_prior_order']),-1)[:-1]
+                # tz = oh_encoding(np.roll(convert_format(row['time_zone']),-1)[:-1],28)
+                prod_info = np.stack([in_order_ord,order_size_ord,index_ord,index_ratio_ord,
+                                      reorder_prod_ord,reorder_prod_ratio_ord,reorder_ord,reorder_ratio_ord,
+                                      days_interval]).transpose()
+                prod_info = np.concatenate([prod_info,order_dow,order_hour,tz,next_order_label],axis=1).astype(np.float16)
+                length = prod_info.shape[0]
+                feature_dim = prod_info.shape[-1] - 1
+                missing_seq = max_len - length
+                if missing_seq > 0:
+                    missing_data = np.zeros([missing_seq,prod_info.shape[1]],dtype=np.float16)
+                    prod_info = np.concatenate([prod_info,missing_data])
+                
+                data_dict[(user,int(product))] = (prod_info,length)
+            pbar.update(1)
 
     user_prod = np.stack(info)
-    return user_prod,data_dict,feature_dim
+    # return user_prod,data_dict,feature_dim
 
 def product_dataloader(inp,
                        data_dict,
@@ -175,12 +184,12 @@ def trainer(data,
         sys.exit(1)
     
     emb_list = ['user_id','product_id','aisle_id','department_id']
-    max_len = 100
-    agg_data = data_processing(data)
+    max_len = data['order_number'].max()
+    agg_data = data_processing(data,save=True)
     data_tr = data_for_training(agg_data,max_len)
     user_prod,prod_feat_dict,feat_dim = data_tr
     up_tr,up_te = train_test_split(user_prod,train_size=train_size,random_state=seed)
-    max_index_info = [data[col].max() for col in emb_list] + [100]
+    max_index_info = [data[col].max() for col in emb_list] + [max_len]
     prod_aisle_dict = prod_data.set_index('product_id')['aisle_id'].to_dict()
     prod_dept_dict = prod_data.set_index('product_id')['department_id'].to_dict()
     train_dl = product_dataloader(up_tr,prod_feat_dict,prod_aisle_dict,prod_dept_dict,batch_size,True,True)
@@ -212,16 +221,18 @@ def trainer(data,
 if __name__ == '__main__':
     import sys
     # try:
-    #     data = pd.read_csv('data/orders_info.csv')
+        # data = pd.read_csv('data/orders_info.csv')
+        # products = pd.read_csv('data/products.csv')
     #     z = data.iloc[:10000]
     # except FileNotFoundError as e:
     #     logger.warning(f"{e}! Please create the data using 'create_merged_data' file first")
     #     sys.exit(1)
-    # products = pd.read_csv('data/products.csv')
-        
+    
     with Timer():
-        # agg_data = data_processing(z)
+        z = data.iloc[1000000:2000000]
+        # agg_data = data_processing(data,True)
         # pre_data = data_for_training(agg_data,max_len=100)
+        
         loss_fn = SeqLogLoss(eps=1e-7)
         trainer(z,
                 products,
@@ -237,5 +248,11 @@ if __name__ == '__main__':
 
 
 #%%
-data['order_number'].max()
+# data['order_number'].max()
+agg_data = pd.read_csv('data/user_product_info.csv')
+# z = agg_data.iloc[:1000]
+# z = data.iloc[1000000:10000000]
+agg_data.loc[10,'reordered']
 
+
+    
